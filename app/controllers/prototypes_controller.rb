b2a32893847a8f21500133d63687a140ef5a9400
yoheitm <tmyoyy9617@gmail.com>
2021-01-14 11:10:08 +0900

    未ログインユーザーが新規投稿ページへ遷移できる不具合の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
